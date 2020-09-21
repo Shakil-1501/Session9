4d8c438dfece5426dbb44237e1ceb72b0dd76044
Shakil-1501 <63920152+Shakil-1501@users.noreply.github.com>
2020-09-21 23:31:02 +0530

    Add files via upload

--- test_session9.py
+++ test_session9.py
@@ -3,6 +3,8 @@ from decimal import Decimal
 import math
 import random
 
+README_CONTENT_CHECK_FOR = ['logged,timed,check_odd_seconds,authenticate,htmlize,html_real,html_sequence']
+
 
 def test_log():
     #a = session9.logged(session9.summation)
@@ -46,4 +48,39 @@ def test_privileges():
     o=session9.info4()
     assert type(l) is tuple and type(m) is tuple and type(n) is tuple and type(o) is str
 
+
+def test_readme_exists():
+    assert os.path.isfile("README.md"), "README.md file missing!"
+
+
+def test_funcation_had_cap_letter():
+    functions = inspect.getmembers(session9, inspect.isfunction )
+    for function in functions:
+        assert len(re.findall('([A-Z])', function[0])) == 0, "You have used Capital letter(s) in your function names"
+
+
+def test_readme_words_counts():
+    readme = open('README.md','r')
+    readme_words = readme.read().split()
+    readme.close()
+    assert len(readme_words) >= 100 , "Kindly define README properly"
+
+
+def test_readme_for_formatting():
+    readme = open('README.md','r')
+    content = readme.read()
+    readme.close()
+    assert content.count('#') >= 5 , "Kindly format the README.md"
+
+
+def test_readme_proper_desscription():
+    READMELOOKSGOOD = True
+    readme = open('README.md','r')
+    readme_words = readme.read().split()
+    readme.close()
+    for words in README_CONTENT_CHECK_FOR:
+        if words not in readme_words:
+            READMELOOKSGOOD = False
+            pass
+    assert READMELOOKSGOOD == True , "You have not defined all functions/classes in README.md"
 
